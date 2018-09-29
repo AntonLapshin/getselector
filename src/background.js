ec91a44d46e3830e58163eda8effb91d4b3bb052
anton_lapshin <antvallap@gmail.com>
2018-09-29 10:32:35 +0200

    update icon for the active tab

--- src/background.js
+++ src/background.js
@@ -1,20 +1,43 @@
-chrome.browserAction.onClicked.addListener(tab => {
-  updateIcon();
-  chrome.tabs.executeScript(
-    tab.ib,
-    {
-      file: "inject.js"
-    },
-    () => {
-      chrome.contextMenus.create({
-        id: "GETSELECTOR_" + details.tabId,
-        title: "Get Unique Selector",
-        contexts: ["editable"],
-        documentUrlPatterns: ["*://*/*"]
-      });
-    }
-  );
+const getState = tabId => 
+  new Promise(resolve => {
+    chrome.tabs.executeScript(
+      tabId,
+      {
+        code: "window.__gs && window.__gs.state"
+      },
+      result => resolve(result[0])
+    );
+  });
+
+const updateIcon = isPressed => chrome.browserAction.setIcon({
+  path: `icon_128${isPressed ? "_pressed" : ""}.png`
 });
+
+!(() => {
+  chrome.browserAction.onClicked.addListener(tab => {
+    chrome.tabs.executeScript(
+      tab.ib,
+      {
+        file: "inject.js"
+      },
+      async () => {
+        const state = await getState(tab.id);
+        updateIcon(state);
+        // chrome.contextMenus.create({
+        //   id: "GETSELECTOR_" + tab.id,
+        //   title: "Get Unique Selector",
+        //   contexts: ["editable"],
+        //   documentUrlPatterns: ["*://*/*"]
+        // });
+      }
+    );
+  });
+
+  chrome.tabs.onActivated.addListener(async tab => {
+    const state = await getState(tab.id);
+    updateIcon(state);    
+  })
+})();
 
 // chrome.contextMenus.onClicked.addListener((info, tab) => {
 //   chrome.tabs.sendMessage(tab.id, { info });
@@ -34,15 +57,5 @@ chrome.browserAction.onClicked.addListener(tab => {
 //     ]);
 //   });
 // });
-
-let pressed = false;
-
-function updateIcon() {
-  pressed = !pressed;
-  console.log(pressed);  
-  chrome.browserAction.setIcon({
-    path: `icon_128${pressed ? "_pressed" : ""}.png`
-  });
-}
 
 // chrome.browserAction.onClicked.addListener(updateIcon);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { init, toggle } from "./app";
 
 !(() => {
-  console.log("GetSelector inject");
   const global = window.__gs = window.__gs || {};
 
   if (global.isInit){
     toggle(global);
   } else {
+    console.log("GetSelector inject");
     init(global);
     toggle(global);
   }
